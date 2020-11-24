cb86b7d563e6bb51b242d974eb40963c18375083
WahyuTI <ardiansyahfarel747@gmail.com>
2020-11-25 00:05:17 +0700

    Hewan pedia

---
+++ app/src/main/java/simple/example/hewanpedia/DataProvider.java
@@ -0,0 +1,92 @@
+package simple.example.hewanpedia;
+
+import android.content.Context;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import simple.example.hewanpedia.model.Anjing;
+import simple.example.hewanpedia.model.Hewan;
+import simple.example.hewanpedia.model.Kucing;
+import simple.example.hewanpedia.model.Kurakura;
+
+public class DataProvider {
+    private static List<Hewan> hewans = new ArrayList<>();
+
+    private static List<Kucing> initDataKucing(Context ctx) {
+        List<Kucing> kucings = new ArrayList<>();
+        kucings.add(new Kucing("Angora", "Turki",
+                "Aslinya berasal dari Ankara, Turki dengan ciri khas berbulu panjang dan lembut", R.drawable.cat_angora));
+        kucings.add(new Kucing("Bengal", "Inggris",
+                "Merupakan hasil persilangan antara Kucing Leopard Asia dengan kucing domestik Inggris", R.drawable.cat_bengal));
+        kucings.add(new Kucing("Birmani", "Birma/Myanmar",
+                "Konon awalnya merupakan ras kucing yand biasa menemani para biksu yang menetap pada kuil di Birma", R.drawable.cat_birman));
+        kucings.add(new Kucing("Persia", "Iran",
+                "Kucing berbulu panjang dengan wajah bundar dan hidung pesek. Awalnya berasal dari Iran kemudian diimpor ke Italia", R.drawable.cat_persia));
+        kucings.add(new Kucing("Siam", "Thailand",
+                "Kucing hasil turunan dari Wichian Maat (Kucing Thailand) dengan ciri khas berbadan panjang dan langsing, wajah lonjong, telinga lebar, dan mata kebiruan", R.drawable.cat_siam));
+        kucings.add(new Kucing("Siberia", "Rusia",
+                "Kucing domestik Rusia dengan bulu lebat dan badan besar tapi sangat lincah dan terkenal dengan kemampuan melompatnya ", R.drawable.cat_siberian));
+        return kucings;
+    }
+
+    private static List<Anjing> initDataAnjing(Context ctx) {
+        List<Anjing> anjings = new ArrayList<>();
+        anjings.add(new Anjing("Bulldog", "Inggris",
+                "Anjing populer yang dikenal dengan badan yang gemuk berotot, wajah seperti kain kusut, dan hidung yang pesek", R.drawable.dog_bulldog));
+        anjings.add(new Anjing("Husky", "Alaska,Siberia,Finlandia (daerah bersalju) ",
+                "Anjing jenis khusus yang awalnya digunakan untuk menarik kereta salju", R.drawable.dog_husky));
+        anjings.add(new Anjing("Kintamani", "Indonesia",
+                "Ras anjing asli pulau Bali ", R.drawable.dog_kintamani));
+        anjings.add(new Anjing("Samoyed", "Rusia",
+                "Anjing yang berasal dari Siberia yang awalnya biasa digunakan untuk menjaga ternak oleh suku Samoyed", R.drawable.dog_samoyed));
+        anjings.add(new Anjing("Shepherd", "Jerman",
+                "Anjing pintar dan kuat, sekarang banyak digunakan sebagai anjing penjaga, pengawal, dan anjing polisi (K9)", R.drawable.dog_shepherd));
+        anjings.add(new Anjing("Shiba", "Jepang",
+                "Anjing dari daerah Shiba ini gesit dan lincah sehingga awalnya banyak digunakan untuk berburu ", R.drawable.dog_shiba));
+        return anjings;
+    }
+    private static List<Kurakura> initDataKurakura(Context ctx) {
+        List<Kurakura> kurakuras = new ArrayList<>();
+        kurakuras.add(new Kurakura("Kura-kura sulcata", "Afrika",
+                "Kura-kura Afrika yang dipacu, juga disebut kura-kura sulcata, ", R.drawable.sulkata));
+        kurakuras.add(new Kurakura("Kura-kura Hermann", "Hermann timur.",
+                "Kura-kura Hermann adalah spesies dalam genus Testudo. ", R.drawable.hermani));
+        kurakuras.add(new Kurakura("Agrionemys horsfieldii", "Rusia",
+                "Kura-kura rusia, juga dikenal dengan nama kura-kura Horsfield atau kura-kura asia tengah,  ", R.drawable.rusia));
+        kurakuras.add(new Kurakura("Kura-kura yunani", "yunani",
+                "Kura-kura Yunani, juga dikenal sebagai kura-kura paha,", R.drawable.yunanai));
+        kurakuras.add(new Kurakura("Kura-kura pardalis", "sabana Afrika",
+                "Kura-kura macan tutul adalah kura-kura besar dan ditandai menarik ", R.drawable.pardalis));
+        kurakuras.add(new Kurakura("Kura-kura bintang india", "india",
+                "Kura-kura bintang India adalah spesies kura-kura terancam yang ditemukan di daerah kering dan menggosok hutan di India,", R.drawable.inda));
+        return kurakuras;
+    }
+
+    private static void initAllHewans(Context ctx) {
+        hewans.addAll(initDataKucing(ctx));
+        hewans.addAll(initDataAnjing(ctx));
+        hewans.addAll(initDataKurakura(ctx));
+    }
+
+    public static List<Hewan> getAllHewan(Context ctx) {
+        if (hewans.size() == 0) {
+            initAllHewans(ctx);
+        }
+        return  hewans;
+    }
+
+    public static List<Hewan> getHewansByTipe(Context ctx, String jenis) {
+        List<Hewan> hewansByType = new ArrayList<>();
+        if (hewans.size() == 0) {
+            initAllHewans(ctx);
+        }
+        for (Hewan h : hewans) {
+            if (h.getJenis().equals(jenis)) {
+                hewansByType.add(h);
+            }
+        }
+        return hewansByType;
+    }
+
+}
---
+++ app/src/main/java/simple/example/hewanpedia/model/Kurakura.java
@@ -0,0 +1,10 @@
+package simple.example.hewanpedia.model;
+
+public class Kurakura extends Hewan{
+
+    public Kurakura(String ras, String asal, String deskripsi, int drawableRes) {
+        super("Kurakura",ras,asal,deskripsi,drawableRes);
+
+    }
+
+}
